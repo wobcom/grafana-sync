4c3b5f8fb774a455f01869157cb0ed742fd885b4
willow <52585984+Kek5chen@users.noreply.github.com>
2024-10-28 13:04:05 +0100

    feat: simple test main for now

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use log::{error, info};
+use crate::config::Config;
+
+mod service;
+mod encrypted_cred;
+mod config;
+mod error;
+
+#[tokio::main]
+async fn main() {
+    match run().await {
+        Err(e) => error!("GraphSync exited with error: {}", e),
+        _ => info!("Exiting."),
+    }
+}
+
+async fn run() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    let config = Config::use_config_file("config.yaml")?;
+
+    config.dbg_print();
+
+    Ok(())
+}
