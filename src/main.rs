271918fde4c811f13285dc36c64dae2ddb3579a8
willow <52585984+Kek5chen@users.noreply.github.com>
2024-10-29 15:21:09 +0100

    feat: use config file from arg

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(duration_constructors)]
 
+use std::env;
 use log::{error, info};
 use tracing::instrument;
 use crate::config::Config;
@@ -25,7 +26,12 @@ async fn main() {
 async fn run() -> Result<(), GSError> {
     env_logger::init();
 
-    let config = Config::use_config_file("config.yaml")?;
+    let args: Vec<String> = env::args().collect();
+    let config_path = args.get(1)
+        .map(|str| str.as_str())
+        .unwrap_or("config.yaml");
+
+    let config = Config::use_config_file(config_path)?;
 
     config.dbg_print();
 
