1ec9dd9e6010ac96d19b12cb6613d7db0137dc58
willow <52585984+Kek5chen@users.noreply.github.com>
2025-02-12 17:39:47 +0100

    multithreaded dashboard syncing

--- src/service.rs
+++ src/service.rs
@@ -2,10 +2,13 @@ use crate::api::dashboards::{Folder, SimpleDashboard};
 use crate::config::Config;
 use crate::error::GSError;
 use chrono::Local;
+use futures::future;
 use log::{debug, error, info};
 use std::collections::{HashMap, HashSet};
+use std::sync::Arc;
 use std::time::Duration;
-use tokio::time::sleep;
+use tokio::task::JoinHandle;
+use tokio::time::{sleep, Instant};
 use tracing::instrument;
 
 #[derive(Debug, Clone)]
@@ -65,46 +68,70 @@ impl SyncService {
         dashboards: &[SimpleDashboard],
         folder_map: &FolderMap,
     ) -> Result<(), GSError> {
-        for dashboard in dashboards.iter() {
-            info!(
-                "Starting replication of dashboard \"{}/{}\"",
-                dashboard.folder_title, dashboard.title
-            );
-
-            let mut full_dashboard = self
+        let mut full_dashboards = Vec::new();
+        for dashboard in dashboards {
+            let full_dashboard = self
                 .config
                 .service
                 .instance_master
                 .get_dashboard_full(&dashboard.uid)
                 .await?;
-            for slave in &self.config.service.instance_slaves {
-                let Some(slave_folder_map) = folder_map.get(slave.base_url()) else {
-                    error!("Slave {} is out of sync (Unauthorized?)", slave.base_url());
-                    continue;
-                };
 
-                let Some(folder) = slave_folder_map.get(&dashboard.folder_title) else {
-                    error!("Slave {} is out of sync. (Unauthorized?)", slave.base_url());
-                    continue;
-                };
-
-                // Override or create new dashboard
-                let old_dashboard = slave
-                    .get_first_dashboard_in_folder_by_name(&folder.uid, &dashboard.title)
-                    .await?;
-                match &old_dashboard {
-                    Some(d) => {
-                        info!("Performing overwriting dashboard sync. Target: {}", d.uid)
-                    }
-                    None => info!("Performing new dashboard sync"),
-                }
-                full_dashboard.sanitize(old_dashboard.as_ref().map(|d| d.uid.as_str()));
-
-                slave
-                    .import_dashboard(&full_dashboard, &folder, true)
-                    .await?;
-            }
+            full_dashboards.push((dashboard.clone(), full_dashboard))
         }
+
+        let instance_slaves = Arc::new(self.config.service.instance_slaves.clone());
+        let folder_map = Arc::new(folder_map.clone());
+
+        let tasks: Vec<JoinHandle<Result<(), GSError>>> = full_dashboards
+            .to_vec()
+            .into_iter()
+            .map(|(dashboard, mut full_dashboard)| {
+                let slaves = instance_slaves.clone();
+                let folder_map = folder_map.clone();
+                tokio::task::spawn(async move {
+                    info!(
+                        "Starting replication of dashboard \"{}/{}\"",
+                        dashboard.folder_title, dashboard.title
+                    );
+
+                    for slave in slaves.iter() {
+                        let Some(slave_folder_map) = folder_map.get(slave.base_url()) else {
+                            error!("Slave {} is out of sync (Unauthorized?)", slave.base_url());
+                            continue;
+                        };
+
+                        let Some(folder) = slave_folder_map.get(&dashboard.folder_title) else {
+                            error!("Slave {} is out of sync. (Unauthorized?)", slave.base_url());
+                            continue;
+                        };
+
+                        // Override or create new dashboard
+                        let old_dashboard = slave
+                            .get_first_dashboard_in_folder_by_name(&folder.uid, &dashboard.title)
+                            .await?;
+                        match &old_dashboard {
+                            Some(d) => {
+                                info!("Performing overwriting dashboard sync. Target: {}", d.uid)
+                            }
+                            None => info!("Performing new dashboard sync"),
+                        }
+                        full_dashboard.sanitize(old_dashboard.as_ref().map(|d| d.uid.as_str()));
+
+                        slave
+                            .import_dashboard(&full_dashboard, &folder, true)
+                            .await?;
+                    }
+
+                    Ok(())
+                })
+            })
+            .collect();
+
+
+        let start = Instant::now();
+        future::join_all(tasks).await;
+        println!("All dashboards were synced in {:?}", start.elapsed());
 
         Ok(())
     }
@@ -141,7 +168,8 @@ impl SyncService {
             debug!("Master Dashboards with synctag: {:?}", &dashboards);
 
             let folder_map = self.replicate_folders_to_slaves(&dashboards).await;
-            self.replicate_dashboards_to_slaves(&dashboards, &folder_map).await?;
+            self.replicate_dashboards_to_slaves(&dashboards, &folder_map)
+                .await?;
 
             self.wait_for_next_sync().await;
         }
