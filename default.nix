1aa4cb761ac318e86e682e547f44fd3a8395d939
willow <52585984+Kek5chen@users.noreply.github.com>
2025-03-11 15:48:10 +0100

    change config to just configFile for secrets

--- default.nix
+++ default.nix
@@ -5,64 +5,17 @@ let
   cfg = config.services.graphsync;
 
   graphsync = pkgs.callPackage ./package.nix {};
-
-  instanceSettings = {
-    url = mkOption {
-      type = types.str;
-      description = "Grafana Base URL";
-    };
-    apiToken = mkOption {
-      type = types.str;
-      description = "Grafana API Token";
-    };
-  };
-
-  configSettings = {
-    syncTag = mkOption {
-      type = types.str;
-      description = "Sync Tag for which boards to sync";
-    };
-
-    instances = mkOption {
-      type = types.attrsOf (types.submodule instanceSettings);
-      description = "instances, with their base url as the key";
-    };
-
-    syncRateMins = mkOption {
-      type = types.int;
-      description = "Synchronization rate in minutes.";
-    };
-  };
-
-  configNix = {
-    service = {
-      sync_tag = cfg.configuration.syncTag;
-      instances = attrsets.mapAttrsToList
-      (name: value: {
-        url = name;
-        api_token = value.apiToken;
-      }) cfg.configuration.instances;
-      sync_rate_mins = cfg.configuration.syncRateMins;
-    };
-  };
-  configYAML = (generators.toYAML {} configNix);
 in
 {
   options = {
     services.graphsync = {
       enable = mkEnableOption "GraphSync";
-      configuration = configSettings;
-    };
-  };
-  config = lib.mkIf cfg.enable {
-    environment.etc = {
-      "graphsync.yaml" = {
-        text = configYAML;
-        mode = "0440";
-        user = "graphsync";
+      configFile = mkOption {
+        type = types.path;
       };
     };
-
+  };
+  config = {
     users.users."graphsync".isNormalUser = true;
 
     systemd.services.graphsync = {
@@ -71,7 +24,7 @@ in
       description = "GraphSync Grafana Syncing Service";
       serviceConfig = {
         Type = "simple";
-        ExecStart = "${graphsync}/bin/graphsync /etc/graphsync/graphsync.yaml";
+        ExecStart = "${graphsync}/bin/graphsync ${cfg.configFile}";
         Restart = "on-failure";
         RestartSec = 5;
         User = "graphsync";
