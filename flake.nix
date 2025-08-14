1440087e903e40ed5fa73c9754cccd1b50f6e64b
willow <me@willow.moe>
2025-08-14 16:52:16 +0200

    fix: remove nix module

--- flake.nix
+++ flake.nix
@@ -34,7 +34,5 @@
       devShells."aarch64-darwin".default = mkDevshell "aarch64-darwin";
       packages."x86_64-linux".default = (mkPkgs "x86_64-linux").callPackage ./package.nix { };
       packages."aarch64-darwin".default = (mkPkgs "aarch64-darwin").callPackage ./package.nix { };
-
-      nixosModules.default = ./default.nix;
     };
 }
